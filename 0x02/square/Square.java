daa5bdf1f25a90ca6a773767f035322727c7d702
Mxn-ptr <maxence0@hotmail.fr>
2025-04-03 13:58:34 +0200

    Add task 1: Function that calculate a square's area and print the result

---
+++ 0x02/square/Square.java
@@ -0,0 +1,7 @@
+public class Square {
+	public static double area(double side) {
+		if (side < 0)
+			throw new java.lang.IllegalArgumentException("Side must have a positive value");
+		return side * side;
+	}
+}
