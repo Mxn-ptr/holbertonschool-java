6fa331adef892d9e94bc1a96004402df863b02d0
Mxn-ptr <maxence0@hotmail.fr>
2025-04-09 16:19:44 +0200

    Modify folder name for checker

---
+++ 0x03/basic_account/BasicBankAccount.java
@@ -0,0 +1,55 @@
+package basic_account;
+import basic_account.exceptions.InvalidOperationException;
+
+public class BasicBankAccount {
+	private String accountNumber;
+	private double balance;
+	private double annualInterestRate;
+
+	public BasicBankAccount(String accountNumber, double annualInterestRate) {
+		this.accountNumber = accountNumber;
+		this.annualInterestRate = annualInterestRate;
+		this.balance = 0;
+	}
+
+	public String getAccountNumber() {
+		return this.accountNumber;
+	}
+
+	public double getBalance() {
+		return this.balance;
+	}
+
+	public double getAnnualInterestRate() {
+		return this.annualInterestRate;
+	}
+
+	public void deposit(double value) throws Exception {
+		if (value <= 0)
+			throw new InvalidOperationException("Deposit amount must be greater than 0");
+		this.balance += value;
+	}
+
+	void withdraw(double value) throws Exception {
+		if (value <= 0)
+			throw new InvalidOperationException("Withdrawal amount must be greater than 0");
+		else if ((this.balance - value) < 0)
+			throw new InvalidOperationException("Withdrawal amount must be less than the current balance");
+		else
+			this.balance -= value;
+	}
+
+	double calculateMonthlyFee() {
+		return Math.min(this.balance / 10, 10);
+	}
+
+	double calculateMonthlyInterest() {
+		if (this.balance >= 0)
+			return this.balance * ((this.annualInterestRate / 12) / 100);
+		return 0;
+	}
+
+	void applyMonthlyUpdate() {
+		this.balance += calculateMonthlyInterest() - calculateMonthlyFee();
+	}
+}
---
+++ 0x03/basic_account/exceptions/InvalidOperationException.java
@@ -0,0 +1,7 @@
+package basic_account.exceptions;
+
+public class InvalidOperationException extends Exception {
+	public InvalidOperationException(String message) {
+		super(message);
+	}
+}
