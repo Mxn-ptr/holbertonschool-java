5503dff5086855ef39e59119f87ac0f01858e56f
Mxn-ptr <maxence0@hotmail.fr>
2025-04-10 17:33:30 +0200

    Add task 1: Modify public attributes for private attributes with getter and setter

---
+++ Classes_Objects/encapsulation/CharacterGame.java
@@ -0,0 +1,23 @@
+public class CharacterGame {
+	private int currentHealth;
+	private String name;
+
+	public CharacterGame() {
+	}
+
+	public int getCurrentHealth() {
+		return this.currentHealth;
+	}
+
+	public void setCurrentHealth(int currentHealth) {
+		this.currentHealth = currentHealth;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
