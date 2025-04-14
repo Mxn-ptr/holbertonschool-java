dca662522c2dc1ef5d82938a46cd319bc6221dbb
Mxn-ptr <maxence0@hotmail.fr>
2025-04-14 10:57:54 +0200

    Add task 3: Add two methods for Rectangle

---
+++ Inheritance/complete_rectangle/Rectangle.java
@@ -0,0 +1,34 @@
+public class Rectangle extends GeometricShape {
+	private double width;
+	private double height;
+
+	public Rectangle() {}
+
+	public double getWidth() {
+		return this.width;
+	}
+
+	public void setWidth(double width) {
+		if (width < 0)
+			throw new IllegalArgumentException("Width must be greater than or equal to 0");
+		this.width = width;
+	}
+
+	public double getHeight() {
+		return this.height;
+	}
+
+	public void setHeight(double height) {
+		if (height < 0)
+			throw new IllegalArgumentException("Height must be greater than or equal to 0");
+		this.height = height;
+	}
+
+	public double area() {
+		return this.width * this.height;
+	}
+
+	public String toString() {
+		return String.format("[Rectange] %.2f / %.2f", this.width, this.height);
+	}
+}
