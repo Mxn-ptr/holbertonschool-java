fdc9953f53667267012aa12bf49e652908732816
Mxn-ptr <maxence0@hotmail.fr>
2025-04-15 15:52:34 +0200

    Modify Slack class with uppercase in notify method

--- Interfaces/workflow/Slack.java
+++ Interfaces/workflow/Slack.java
@@ -1,5 +1,5 @@
 public class Slack implements NotificationChannel {
 	public void notify(Message message) {
-		System.out.printf(("[Slack] {%s} - %s\n"), message.getMessageType().toString(), message.getText());
+		System.out.printf(("[SLACK] {%s} - %s\n"), message.getMessageType().toString(), message.getText());
 	}
 }
