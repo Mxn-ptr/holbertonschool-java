e2b2a67a2e03b6a6fc7052725f46001c1d1d651f
Mxn-ptr <maxence0@hotmail.fr>
2025-05-21 16:55:42 +0200

    Add task 1

---
+++ Streams_Collector_Optional_Exercices/people_count_position/PeopleQuery.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class PeopleQuery {
+    public static TreeMap<String, TreeSet<Person>> getPeopleGroupedByPositionInReverseOrder(List<Person> persons) {
+        return persons.stream().collect(Collectors.groupingBy(
+                Person::getPosition,
+                () -> new TreeMap<>(Comparator.reverseOrder()),
+                Collectors.toCollection(() -> new TreeSet<>(Comparator.comparing(Person::getCode))))
+        );
+    }
+
+    public static Map<String, Long> getPeopleCountByPosition(List<Person> persons) {
+        return persons.stream().collect(Collectors.groupingBy(Person::getPosition, Collectors.counting()));
+    }
+}
